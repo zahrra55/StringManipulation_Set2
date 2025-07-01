adfc0dbe6d67b07f8f45f0142bb2f0146881d463
mustafa Kenany <mustafa.kenany2022@gmail.com>
2025-07-02 00:37:31 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using strings;
+
+
+Console.Write("Please input valid string to see how many chars include:");
+string words = Console.ReadLine();
+Console.WriteLine($"Chars Pcs are: {helper.CountCharsOfinputString(words)}");
---
+++ helper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace strings
+{
+    public class helper
+    {
+
+        public static int CountCharsOfinputString(string _string)
+        {
+            //simple method return length of input string using Length Method
+            try
+            {
+                while (_string == string.Empty)
+                {
+                    Console.Write("RE input string in Valid Format: ");
+                }
+                return _string.Length;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There is Error in Input string maybe null or invalid format");
+            }
+            return 0;
+        }
+    }
+}
