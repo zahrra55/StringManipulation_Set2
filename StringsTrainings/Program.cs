2977990e372a0629b18bbc029d790e830a3057c3
MoOsA5 <moosasumer01@gmail.com>
2025-07-01 17:23:41 +0300

    New String tasks and some of its solutions

---
+++ StringsTrainings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace StringsTrainings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //calling WordApearingCounts method and print the count
+            Console.WriteLine(Solutions.WordApearingCounts("apple"));
+
+            //calling ConvertToUpperCase method and print the result
+            Console.WriteLine(Solutions.ConvertToUpperCase("moosa"));
+
+            //calling ConvertToLowerCase method and print the result
+            Console.WriteLine(Solutions.ConvertToLowerCase("MOOSA"));
+
+            //calling IsStartsWith method and print the result
+            Console.WriteLine(Solutions.IsStartsWith("wel"));
+
+            //calling IsStartsWith method and print the result
+            Console.WriteLine(Solutions.IsEndsWith("txt"));
+        }
+    }
+}
---
+++ StringsTrainings/Solutions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+
+namespace StringsTrainings
+{
+    class Solutions
+    {
+        //دالة تحسب عدد تكرار كلمة معينة
+        //نوزع الكلمات بمصفوفة وباستخدام اللوب نفحص الكلمات الي تشبه الكلمة المراد معرفة تكرارها
+        public static int WordApearingCounts(String word)
+        {
+            String sentence = "apple banana apple orange apple";
+            String[] wordsAfterSplit;
+            wordsAfterSplit = sentence.Split(' ');
+            int count=0;
+            foreach (String str in wordsAfterSplit)
+            {
+                if (str == word)
+                    count++;
+            }
+            return count;
+        }
+
+        //دالة تحول كل حروف الكلمة الى حروف كبيرة
+        public static String ConvertToUpperCase(String word)
+        {
+            return word.ToUpper();
+        }
+
+        //دالة تحول كل حروف الكلمة الى حروف صغيرة
+        public static String ConvertToLowerCase(String word)
+        {
+            return word.ToLower();
+        }
+
+        //دالة لفحص اذا كانت الكلمة او الجملة تبدأ بحرف او كلمة معينة
+        public static bool IsStartsWith(String word)
+        {
+            String sentance = "welcome";
+            return sentance.StartsWith(word) ? true : false;
+        }
+
+        //دالة لفحص اذا كانت الكلمة او الجملة تنتهي بحرف او كلمة معينة
+        public static bool IsEndsWith(String word)
+        {
+            String sentance = "file.txt";
+            return sentance.EndsWith(word) ? true : false;
+        }
+
+        // تمرين 6 و 7
+
+        //دالة لتحويل الحرف الاول من الكلمة الى حرف كبير
+        public static String CapitalizeFirstLetter (String word)
+        {
+            char[] newWord;
+            foreach(char i in word)
+            {
+                if (i == 0)
+                    newWord[i] = word[i];
+
+            }
+
+        }
+    }
+
+}
