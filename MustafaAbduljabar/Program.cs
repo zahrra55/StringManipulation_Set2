3b9b917c080d05c979e19a7722cddaf3a840b218
Zahraa <shugo7385@gmail.com>
2025-07-06 18:18:23 +0300

    Delete MustafaAbduljabar directory

--- MustafaAbduljabar/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using strings;
-
-Console.Write("Input string to Process: ");
-string input = Console.ReadLine();
-//Console.Write("Input prefex to check: ");
-//string prefex = Console.ReadLine();
-
-Console.WriteLine($"check IsDigit: {StringHelper.FindAllDigits(input)}");
